a0a987c7afa95f0b752e44f6fab71eb5b8e6da1a
Rodolfo Castelo Méndez <rodolfo.castelo@outlook.com>
2024-01-23 13:58:56 +0100

    feat: First version

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v1.0.0
+- Added more functions
+
+# v0.0.1
+- Initial version
---
+++ doc.go
@@ -0,0 +1,8 @@
+// Copyright 2024 Croonix. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+This module contains helper functions primary focused on Google Cloud Platform
+*/
+package helper
