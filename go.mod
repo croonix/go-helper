bf6c8730fea1189a69b6e55c8e0e544428d2fb6e
Rodolfo Castelo Méndez <rodolfo.castelo@outlook.com>
2024-02-23 13:51:28 +0100

    feat: Added AddServiceAccountUserRole & RemoveServiceAccountUserRole

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	cloud.google.com/go/logging v1.9.0
 	github.com/go-sql-driver/mysql v1.7.1
 	golang.org/x/oauth2 v0.17.0
-	google.golang.org/api v0.165.0
+	google.golang.org/api v0.166.0
 )
 
 require (
@@ -20,6 +20,7 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
@@ -38,6 +39,6 @@ require (
 	google.golang.org/genproto v0.0.0-20240221002015-b0ce06bbee7c // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240221002015-b0ce06bbee7c // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240221002015-b0ce06bbee7c // indirect
-	google.golang.org/grpc v1.61.1 // indirect
+	google.golang.org/grpc v1.62.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
--- helper.go
+++ helper.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
@@ -19,6 +20,7 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/iam/v1"
 	"google.golang.org/api/impersonate"
 	"google.golang.org/api/option"
 )
@@ -78,6 +80,108 @@ func ImpersonateSA(context context.Context, serviceAccount string, scope string)
 		return
 	}
 	return token, nil
+}
+
+func AddServiceAccountUserRole(impersonateAccount string, targetAccount string, principalAccount string) error {
+	return setServiceAccountUserRole(impersonateAccount, targetAccount, principalAccount, true)
+}
+
+func RemoveServiceAccountUserRole(impersonateAccount string, targetAccount string, principalAccount string) error {
+	return setServiceAccountUserRole(impersonateAccount, targetAccount, principalAccount, false)
+}
+
+func setServiceAccountUserRole(impersonateAccount string, targetAccount string, principalAccount string, enabled bool) error {
+	var (
+		ctx           = context.Background()
+		functionScope = "https://www.googleapis.com/auth/iam"
+		principalId   = fmt.Sprintf("serviceAccount:%s", principalAccount)
+		targetId      = fmt.Sprintf("projects/%s/serviceAccounts/%s", getProjectFromServiceAccount(targetAccount), targetAccount)
+		role          = "roles/iam.serviceAccountUser"
+		iamClient     *iam.Service
+		token         oauth2.TokenSource
+	)
+
+	if impersonateAccount != "" {
+		var err error
+		token, err = ImpersonateSA(ctx, impersonateAccount, functionScope)
+		if err != nil {
+			return err
+		}
+
+		iamClient, err = iam.NewService(ctx, option.WithTokenSource(token))
+		if err != nil {
+			return err
+		}
+	} else {
+		var err error
+		iamClient, err = iam.NewService(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	currentPolicy, err := iamClient.Projects.ServiceAccounts.GetIamPolicy(targetId).Do()
+	if err != nil {
+		return err
+	}
+
+	if enabled {
+		newBinding := &iam.Binding{
+			Role:    role,
+			Members: []string{principalId},
+		}
+
+		_, err = iamClient.Projects.ServiceAccounts.SetIamPolicy(targetId, &iam.SetIamPolicyRequest{
+			Policy: &iam.Policy{
+				Bindings: append(currentPolicy.Bindings, newBinding),
+			},
+		}).Do()
+		if err != nil {
+			return err
+		}
+		return nil
+	} else {
+		var updatedBindings []*iam.Binding
+		for _, binding := range currentPolicy.Bindings {
+			if binding.Role != role {
+				updatedBindings = append(updatedBindings, &iam.Binding{
+					Role:    binding.Role,
+					Members: binding.Members,
+				})
+				continue
+			}
+			var membersWithoutUser []string
+			for _, member := range binding.Members {
+				if member != principalId {
+					membersWithoutUser = append(membersWithoutUser, member)
+					updatedBindings = append(updatedBindings, &iam.Binding{
+						Role:    binding.Role,
+						Members: membersWithoutUser,
+					})
+				}
+			}
+		}
+		_, err = iamClient.Projects.ServiceAccounts.SetIamPolicy(targetId, &iam.SetIamPolicyRequest{
+			Policy: &iam.Policy{
+				Bindings: updatedBindings,
+			},
+		}).Do()
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func getProjectFromServiceAccount(serviceAccount string) string {
+	regex := regexp.MustCompile(`@([^\.]+)\.iam\.gserviceaccount\.com`)
+	matches := regex.FindStringSubmatch(serviceAccount)
+
+	if len(matches) >= 2 {
+		return matches[1]
+	} else {
+		return "-"
+	}
 }
 
 func getGCPProject() string {
